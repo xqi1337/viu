93654be74fd001f0b9bc194d6727f7f7e437109e
Benexl <benextempest@gmail.com>
2025-02-22 12:58:45 +0300

    feat: enhance login experience

--- fastanime/cli/commands/anilist/login.py
+++ fastanime/cli/commands/anilist/login.py
@@ -11,17 +11,20 @@ if TYPE_CHECKING:
 @click.option("--erase", "-e", help="Erase your login details", is_flag=True)
 @click.pass_obj
 def login(config: "Config", status, erase):
-    from sys import exit
-    from sys import platform
     from os import path
+    from sys import exit
 
     from rich import print
     from rich.prompt import Confirm, Prompt
 
+    from ....constants import S_PLATFORM
+
     if status:
         is_logged_in = True if config.user else False
         message = (
-            "You are logged in :smile:" if is_logged_in else "You aren't logged in :cry:"
+            "You are logged in :smile:"
+            if is_logged_in
+            else "You aren't logged in :cry:"
         )
         print(message)
         print(config.user)
@@ -49,16 +52,17 @@ def login(config: "Config", status, erase):
             f"A browser session will be opened ( [link]{config.fastanime_anilist_app_login_url}[/link] )",
         )
         token = ""
-        if (platform.startswith('darwin')):
+        if S_PLATFORM.startswith("darwin"):
             anilist_key_file_path = path.expanduser("~") + "/Downloads/anilist_key.txt"
             launch(config.fastanime_anilist_app_login_url, wait=False)
-            Prompt.ask("MacOS detected.\nPress any key once the token provided has been pasted into " + anilist_key_file_path)
+            Prompt.ask(
+                "MacOS detected.\nPress any key once the token provided has been pasted into "
+                + anilist_key_file_path
+            )
             with open(anilist_key_file_path, "r") as key_file:
                 token = key_file.read().strip()
         else:
-            Prompt.ask("OS which is not MacOS detected.")
-            launch(config.fastanime_anilist_app_login_url, wait=True)
-            print("Please paste the token provided here")
+            launch(config.fastanime_anilist_app_login_url, wait=False)
             token = Prompt.ask("Enter token")
         user = AniList.login_user(token)
         if not user:
