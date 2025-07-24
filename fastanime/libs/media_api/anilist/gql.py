e87e4e56398bfd3d82b98f90f9d1ba0fa9e4e554
Benexl <benextempest@gmail.com>
2025-07-24 19:15:04 +0300

    refactor: update GraphQL query paths for consistency and clarity

--- fastanime/libs/media_api/anilist/gql.py
+++ fastanime/libs/media_api/anilist/gql.py
@@ -8,13 +8,13 @@ _MUTATIONS_PATH = _ANILIST_PATH / "mutations"
 SEARCH_MEDIA = _QUERIES_PATH / "search.gql"
 SEARCH_USER_MEDIA_LIST = _QUERIES_PATH / "media-list.gql"
 
-GET_AIRING_SCHEDULE = _QUERIES_PATH / "airing.gql"
-GET_CHARACTERS = _QUERIES_PATH / "character.gql"
-GET_MEDIA_LIST_ITEM = _QUERIES_PATH / "get-medialist-item.gql"
-GET_LOGGED_IN_USER = _QUERIES_PATH / "logged-in-user.gql"
+GET_AIRING_SCHEDULE = _QUERIES_PATH / "media-airing-schedule.gql"
+GET_MEDIA_CHARACTERS = _QUERIES_PATH / "media-characters.gql"
+GET_MEDIA_RECOMMENDATIONS = _QUERIES_PATH / "media-recommendations.gql"
 GET_MEDIA_RELATIONS = _QUERIES_PATH / "media-relations.gql"
+
+GET_LOGGED_IN_USER = _QUERIES_PATH / "logged-in-user.gql"
 GET_NOTIFICATIONS = _QUERIES_PATH / "notifications.gql"
-GET_RECOMMENDATIONS = _QUERIES_PATH / "recommended.gql"
 GET_REVIEWS = _QUERIES_PATH / "reviews.gql"
 GET_USER_INFO = _QUERIES_PATH / "user-info.gql"
 
