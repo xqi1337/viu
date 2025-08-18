a10e56cb6f3ef6d9fa36f4bc75a1760a2037ec47
Benexl <benextempest@gmail.com>
2025-08-18 13:19:56 +0300

    refactor:set min supported python version to 3.11

--- viu_media/__init__.py
+++ viu_media/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-if sys.version_info < (3, 10):
+if sys.version_info < (3, 11):
     raise ImportError(
         "You are using an unsupported version of Python. Only Python versions 3.10 and above are supported by Viu"
     )  # noqa: F541
